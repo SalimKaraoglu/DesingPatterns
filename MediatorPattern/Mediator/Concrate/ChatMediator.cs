f898a033edfc93c2e05efbe09d09400de8c35a09
Salim Karaoğlu <96123185+SalimKaraoglu@users.noreply.github.com>
2023-11-21 15:46:08 +0300

    Add project files.

---
+++ MediatorPattern/Mediator/Concrate/ChatMediator.cs
@@ -0,0 +1,35 @@
+﻿using MediatorPattern.Interface;
+using MediatorPattern.Mediator.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatorPattern.Mediator.Concrate
+{
+    public class ChatMediator : IChatMediator
+    {
+        private List<IUser> users;
+        public ChatMediator()
+        {
+            users = new List<IUser>();
+        }
+        public void AddUser(IUser user)
+        {
+            users.Add(user);
+        }
+
+        public void SendMessage(string message, IUser sender)
+        {
+            foreach (var user in users)
+            {
+                if (user != sender)
+                {
+                    user.ReceiveMessage(message);
+                }
+
+            }
+        }
+    }
+}
---
+++ MediatorPattern/Program.cs
@@ -0,0 +1,31 @@
+﻿using MediatorPattern.Concrate;
+using MediatorPattern.Interface;
+using MediatorPattern.Mediator.Concrate;
+using MediatorPattern.Mediator.Interface;
+
+namespace MediatorPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IChatMediator chatMediator = new  ChatMediator();
+
+            IUser arda = new BasicUser("jazox" , chatMediator);
+            IUser hazar = new GoldUser("catch-up" , chatMediator);
+            IUser idil = new GoldUser("Idile" , chatMediator);
+
+            chatMediator.AddUser(arda);
+            chatMediator.AddUser(hazar);
+            chatMediator.AddUser(idil);
+
+            Console.WriteLine("Hazarın mesajı: ");
+            hazar.SendMessage("Tavuk - Pilav ketçapsız yenmez! Ketçapsız yiyen ağzının tadını bilmiyordur!");
+
+            Console.WriteLine("\nArdanın mesajı: ");
+            arda.SendMessage("Sen aklına koyduğun herşeyi başarırsın!");
+
+
+        }
+    }
+}
