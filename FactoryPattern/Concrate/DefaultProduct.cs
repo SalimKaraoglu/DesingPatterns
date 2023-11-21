b78b04c0a5265d4aedb2198b61b4b9d204a64974
Salim Karaoğlu <96123185+SalimKaraoglu@users.noreply.github.com>
2023-11-21 17:17:58 +0300

    FactoryPattern

---
+++ FactoryPattern/Concrate/DefaultProduct.cs
@@ -0,0 +1,14 @@
+﻿using FactoryPattern.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern.Concrate
+{
+    public class DefaultProduct : IProduct
+    {
+        public string ShipFrom() => "Shipping isn't available...";
+    }
+}
